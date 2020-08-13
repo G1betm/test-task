3410d82fede71eeeb7e5c48633f346ced5f4ea40
G1betm <korvinamber1@mail.ru>
2020-08-13 22:30:36 +0500

    add mobile adaptation

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+    const navBtn = document.querySelector(".nav__btn");
+    const nav = document.querySelector(".nav__list");
 
-  const navBtn = document.querySelector('.nav__btn');
-  const nav = document.querySelector('.nav__list');
-
-  navBtn.addEventListener('click', function() {
-    navBtn.classList.toggle('close');
-    nav.classList.toggle('active');
-  })
+    navBtn.addEventListener("click", function () {
+        navBtn.classList.toggle("close");
+        nav.classList.toggle("active");
+    });
 });
