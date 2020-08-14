2d316adfc9ae0172fc9a7d4754a267fb64214d62
G1betm <korvinamber1@mail.ru>
2020-08-14 12:07:14 +0500

    add form validation

--- main.js
+++ main.js
@@ -7,3 +7,32 @@ document.addEventListener("DOMContentLoaded", function () {
         nav.classList.toggle("active");
     });
 });
+
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = "form-control error";
+    const small = formControl.querySelector("small");
+    small.innerText = message;
+}
+
+function showSucces(input) {
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
+
+function checkEmail(input) {
+    const regular = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (regular.test(input.value.trim())) {
+        showSucces(input);
+    } else {
+        showError(input, "Email is invalid");
+    }
+}
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    checkEmail(email);
+});
